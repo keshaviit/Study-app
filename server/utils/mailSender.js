0974b0a2051c9125d7da9474040171917deddcfb
Keshav_Goyal <keshav.goyal@fiellements.com>
2026-01-31 23:32:21 +0530

    Switch email to Resend API

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,28 +1,20 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const mailSender = async (email, title, body) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"StudyNotion" <${process.env.MAIL_USER}>`,
+    const data = await resend.emails.send({
+      from: process.env.RESEND_FROM,
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("MAIL SENT:", info.messageId);
-    return info;
+    console.log("Email sent:", data);
+    return data;
   } catch (error) {
-    console.error("MAIL ERROR:", error);
+    console.error("Resend error:", error);
     throw error;
   }
 };
